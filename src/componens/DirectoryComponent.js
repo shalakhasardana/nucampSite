ef39d41480bc02069d672d78b229195edbf2a901
unknown <shalakhasardana18@gmail.com>
2020-08-09 19:01:08 -0700

    Exercise Presentational and Container Component

--- src/componens/DirectoryComponent.js
+++ src/componens/DirectoryComponent.js
@@ -1,25 +1,12 @@
 import React, { Component } from "react";
-
-import CampsiteInfo from "./CampsiteInfoComponent";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 
 class Directory extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedCampsite: null,
-    };
-  }
-
-  onCampsiteSelect(campsite) {
-    this.setState({ selectedCampsite: campsite });
-  }
-
   render() {
     const directory = this.props.campsites.map((campsite) => {
       return (
         <div key={campsite.id} className="col-md-5 m-1">
-          <Card onClick={() => this.onCampsiteSelect(campsite)}>
+          <Card onClick={() => this.props.onClick(campsite.id)}>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
               <CardTitle>{campsite.name}</CardTitle>
@@ -32,7 +19,7 @@ class Directory extends Component {
     return (
       <div className="container">
         <div className="row">{directory}</div>
-        <CampsiteInfo campsite={this.state.selectedCampsite} />
+
         {/* Use props and state */}
         {/* <ExampleParentComponent/> */}
       </div>
