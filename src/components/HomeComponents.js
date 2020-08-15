deccfc5eab66bcfe5a4c14bc7714e2fe6824bba9
unknown <shalakhasardana18@gmail.com>
2020-08-15 01:21:33 -0700

    React Router Parameters

--- src/components/HomeComponents.js
+++ src/components/HomeComponents.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';}
 
 function Home(props) {
   return (
     <div className="container">
       <h4>Home</h4>
     </div>
+  );
+}
+
+function Home(props) {
+  return (
+      <div className="container">
+          <div className="row">
+              <div className="col-md m-1">
+                  <RenderCard item={props.campsite} />
+              </div>
+              <div className="col-md m-1">
+                  <RenderCard item={props.promotion} />
+              </div>
+              <div className="col-md m-1">
+                  <RenderCard item={props.partner} />
+              </div>
+          </div>
+      </div>
   );
 }
 
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,29 +6,67 @@ import { CAMPSITES } from "../shared/campsites";
 import Header from "./HeaderComponents";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponents";
+import Contact from "./ContactComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
+import { COMMENTS } from "../shared/comments";
+import { PARTNERS } from "../shared/partners";
+import { PROMOTIONS } from "../shared/promotions";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      comments: COMMENTS,
+      partners: PARTNERS,
+      promotions: PROMOTIONS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          campsite={
+            this.state.campsites.filter((campsite) => campsite.featured)[0]
+          }
+          promotions={
+            this.state.promotions.filter((promotion) => promotion.featured)[0]
+          }
+          partners={
+            this.state.partners.filter((partner) => partner.featured)[0]
+          }
+        />
+      );
     };
+
+    const CampsiteWithId = ({ match }) => {
+      return (
+        <CampsiteInfo
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === +match.params.campsiteId
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.campsiteId === +match.params.campsiteId
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route
-            exact path="/directory"
+            exact
+            path="/directory"
             render={() => <Directory campsites={this.state.campsites} />}
           />
+          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
